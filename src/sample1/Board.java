2ae7cffd6400ee7528a8fb88385fbac7170062e2
mijung-park <mezzang.park@gmail.com>
2021-05-26 17:53:59 +0900

    20210526

---
+++ src/sample1/Board.java
@@ -0,0 +1,28 @@
+package sample1;
+
+public class Board {
+	private String title;
+	private String body;
+	private String writer;
+	
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	
+}
