fc322620e4b674aa049e746d1db0ea562c241806
mijung-park <mezzang.park@gmail.com>
2021-05-28 16:10:00 +0900

    20210528

---
+++ src/ch14/bean/Employee.java
@@ -0,0 +1,31 @@
+package ch14.bean;
+
+public class Employee {
+	private String num;
+	private String lastName;
+	private String firstName;
+	
+	
+	public String getNum() {
+		return num;
+	}
+	public void setNum(String num) {
+		this.num = num;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+
+
+	
+}
